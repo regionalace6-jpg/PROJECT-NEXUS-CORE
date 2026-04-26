a5a8b5eab9d019e468616413d61085fb4581014d
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:23:53 +0530

    Update roles.js

--- nexus-core/config/roles.js
+++ nexus-core/config/roles.js
@@ -1,1 +1,15 @@
+module.exports = {
+  roles: {
+    OWNER: ["YOUR_DISCORD_ID"],
+    ADMIN: [],
+    ANALYST: []
+  },
 
+  permissions: {
+    report: ["OWNER", "ADMIN", "ANALYST"],
+    blacklist: ["OWNER", "ADMIN"],
+    altcheck: ["OWNER", "ADMIN", "ANALYST"],
+    alert: ["OWNER", "ADMIN", "ANALYST"],
+    presence: ["OWNER", "ADMIN", "ANALYST"]
+  }
+};
