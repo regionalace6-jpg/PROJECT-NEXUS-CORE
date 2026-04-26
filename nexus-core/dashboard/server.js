ea8650385e7ca557a9cd874d42017d74abacb740
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:29:03 +0530

    Update server.js

--- nexus-core/dashboard/server.js
+++ nexus-core/dashboard/server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const routes = require("./routes");
 
+const app = express();
+app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.send("NEXUS Dashboard Running");
+});
+
+app.listen(3000, () => {
+  console.log("Dashboard running on 3000");
+});
