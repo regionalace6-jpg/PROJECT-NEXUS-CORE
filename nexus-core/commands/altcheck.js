81d5aab4d11ebbc6a86817c5bcf503f1e03161ee
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:22:32 +0530

    Update altcheck.js

--- nexus-core/commands/altcheck.js
+++ nexus-core/commands/altcheck.js
@@ -1,1 +1,15 @@
+const { altCheck } = require("../services/altEngine");
 
+module.exports = {
+  name: "altcheck",
+
+  async execute(message) {
+
+    const user = message.mentions.users.first() || message.author;
+    const suspects = await altCheck(user.id);
+
+    if (!suspects.length) return message.reply("No alt patterns.");
+
+    message.reply(`Possible alts:\n${suspects.join("\n")}`);
+  }
+};
