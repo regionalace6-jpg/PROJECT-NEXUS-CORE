7933d3bab23da1d1af16ee6d8290e9ebbe82b17c
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:21:22 +0530

    Update embed.js

--- nexus-core/utils/embed.js
+++ nexus-core/utils/embed.js
@@ -1,1 +1,10 @@
+const { EmbedBuilder } = require("discord.js");
 
+function embed(title) {
+  return new EmbedBuilder()
+    .setColor("#2b2d31")
+    .setTitle(title)
+    .setTimestamp();
+}
+
+module.exports = { embed };
