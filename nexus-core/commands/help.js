f886454a00f9a2ba7b054ef722c2c17c6ff40101
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:21:45 +0530

    Update help.js

--- nexus-core/commands/help.js
+++ nexus-core/commands/help.js
@@ -1,1 +1,19 @@
+const { embed } = require("../utils/embed");
 
+module.exports = {
+  name: "help",
+  execute(message) {
+
+    const e = embed("NEXUS CORE")
+      .setDescription("Commands")
+      .addFields(
+        { name: ".report", value: "Full report" },
+        { name: ".presence", value: "Server footprint" },
+        { name: ".alert", value: "Spike detection" },
+        { name: ".altcheck", value: "Alt scan" },
+        { name: ".blacklist", value: "Blacklist user" }
+      );
+
+    message.reply({ embeds: [e] });
+  }
+};
