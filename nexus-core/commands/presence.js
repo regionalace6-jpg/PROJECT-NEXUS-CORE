ae87a3ddbb6ff6dd4874996678bd453db42ed1e0
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:22:19 +0530

    Update presence.js

--- nexus-core/commands/presence.js
+++ nexus-core/commands/presence.js
@@ -1,1 +1,13 @@
+const { getUserStats } = require("../services/intelEngine");
 
+module.exports = {
+  name: "presence",
+
+  async execute(message) {
+
+    const user = message.mentions.users.first() || message.author;
+    const stats = await getUserStats(user.id);
+
+    message.reply(`Seen in ${stats.servers} servers.`);
+  }
+};
