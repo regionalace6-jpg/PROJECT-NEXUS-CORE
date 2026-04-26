d881f887c1d5bde87cee8d7227735df9eb92fa55
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:21:30 +0530

    Update chart.js

--- nexus-core/utils/chart.js
+++ nexus-core/utils/chart.js
@@ -1,1 +1,11 @@
+function generateChart(labels, data) {
+  return `https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify({
+    type: "line",
+    data: {
+      labels,
+      datasets: [{ label: "Activity", data }]
+    }
+  }))}`;
+}
 
+module.exports = { generateChart };
