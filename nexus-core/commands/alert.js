9908f3226c4e9f19d8fcd7b60489f1ce09886319
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:28:10 +0530

    Update alert.js

--- nexus-core/commands/alert.js
+++ nexus-core/commands/alert.js
@@ -1,9 +1,11 @@
 const { checkSpike } = require("../services/alertService");
+const { auth } = require("../middleware/auth");
 
 module.exports = {
   name: "alert",
 
   async execute(message) {
+    if (!auth("alert")(message)) return;
 
     const user = message.mentions.users.first() || message.author;
 
