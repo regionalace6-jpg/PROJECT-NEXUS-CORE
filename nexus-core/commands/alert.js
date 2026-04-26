7bf4c470e0df366ce37f3316adf3c2a6ca79cd18
regionalace6-jpg <regionalace6@gmail.com>
2026-04-26 14:22:11 +0530

    Update alert.js

--- nexus-core/commands/alert.js
+++ nexus-core/commands/alert.js
@@ -1,1 +1,14 @@
+const { checkSpike } = require("../services/alertService");
 
+module.exports = {
+  name: "alert",
+
+  async execute(message) {
+
+    const user = message.mentions.users.first() || message.author;
+
+    const spike = await checkSpike(user.id);
+
+    message.reply(spike ? "Spike detected." : "Normal activity.");
+  }
+};
